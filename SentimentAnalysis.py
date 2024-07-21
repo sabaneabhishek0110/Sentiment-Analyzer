27e8c9b94100b614ffebeb75c766d4482e30d09d
Jyoti Sabane <sabanejyoti02@gmail.com>
2024-07-21 06:26:14 -0700

    sentiment analysis completed

---
+++ SentimentAnalysis.py
@@ -0,0 +1,214 @@
+import sys
+import string
+import json
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from collections import Counter
+import matplotlib.pyplot as plt
+import csv
+import speech_recognition as sr
+# import pyttsx3
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('vader_lexicon')
+
+nrc_lexicon = {}
+# Load the NRC Emotion Lexicon
+with open('E:/abhishek/SA/Sentiment Analysis New 13-7-2024/Sentiment Analysis/NRC-Emotion-Lexicon/NRC-Emotion-Lexicon/NRC-Emotion-Lexicon-Wordlevel-v0.92.txt', 'r') as file:
+    reader = csv.reader(file, delimiter='\t')
+    for row in reader:
+        word, emotion, association = row
+        if word not in nrc_lexicon:
+            nrc_lexicon[word] = {}
+        nrc_lexicon[word][emotion] = int(association)
+
+def text_analyze(text) :
+    lowercase = text.lower()
+    cleaned_text = lowercase.translate(str.maketrans(" ", " ", string.punctuation))
+    
+    Tokenised_words = word_tokenize(cleaned_text,"english")
+    final_words = []
+    for word in Tokenised_words:
+        if word not in stopwords.words("english"):
+            final_words.append(word)
+        
+        emotions_count = {
+            'anger': 0,
+            'anticipation': 0,
+            'disgust': 0,
+            'fear': 0,
+            'joy': 0,
+            'sadness': 0,
+            'surprise': 0,
+            'trust': 0,
+            'positive': 0,
+            'negative': 0
+        }
+
+    # Analyze emotions in text
+    for word in final_words:
+        if word in nrc_lexicon:
+            for emotion, association in nrc_lexicon[word].items():
+                if association == 1:
+                    emotions_count[emotion] += 1
+
+    w = emotions_count
+    # print(json.dumps(w))
+    # print(emotions_count)
+
+    score = SentimentIntensityAnalyzer().polarity_scores(cleaned_text)
+    neg = score['neg']
+    pos = score['pos']
+    if(pos>neg):
+        Overall_sentiment = "Positive"
+    elif(neg>pos):
+        Overall_sentiment = "Negative"
+    else:
+        Overall_sentiment = "Neutral"
+
+    # overall_sentiment_analysis(cleaned_text)
+    # fig, ax1 = plt.subplots()
+    # ax1.bar(w.keys(), w.values())
+    # fig.autofmt_xdate()
+    # plt.savefig('graph.png')
+    # plt.show()
+
+    return {
+        # 'emotions_count': emotions_count,
+        'emotion_distribution': dict(w),
+        'overall_sentiment': Overall_sentiment
+    }
+
+def speech_recognize():
+    r = sr.Recognizer()
+    with sr.Microphone() as source2:
+        print("Speech is Recognising.....")
+        # Prepare Recognizer to recieve input
+        r.adjust_for_ambient_noise(source2, duration=0.2)
+        # Listen for users input
+        audio2 = r.listen(source2)
+        # Using google to recognize audio
+        try:
+            MyText = r.recognize_google(audio2)
+            return MyText
+
+        except sr.RequestError as e:
+            print("could not request results. {0}".format(e))
+        except sr.UnknownValueError:
+            print("Unknown erroor occured.")
+
+        return None
+
+n='2'
+def main():
+    # print("Select Your Choice Below and Enter Accordingly: ")
+    # print("Speech Recognition : 1")
+    # print("From File : 2")
+    # n = input("Your Choice : ")
+    # n=sys.argv[2]
+    # n = sys.argv[0]
+    if(n=='1'):
+        # Initialize the recogniser
+        
+        text = speech_recognize()
+        def output(text):
+            f = open("Data/Output.txt", 'w')
+            f.write(text)
+            f.write("\n")
+            f.close()
+            return
+
+        # # while(1):
+        text = input()
+        if (text):
+            output(text)
+            print("speech recognised")
+            text = open('Output.txt', encoding='UTF-8').read()
+            result = text_analyze(text)
+            print(json.dumps(result))
+            
+
+            # overall_sentiment_analysis(cleaned_text)
+            # fig, ax1 = plt.subplots()
+            # ax1.bar(w.keys(), w.values())
+            # fig.autofmt_xdate()
+            # plt.savefig('graph.png')
+            # plt.show()\
+            # plt.close(fig)
+        else:
+            print("speech is not recognized")
+
+    elif(n=='2'):
+        # Removing all punctuations from text and making text lowercase
+        text = open('../Sentiment Analysis/Data/content.txt', encoding='UTF-8').read()
+        result = text_analyze(text)
+        print(json.dumps(result))
+
+        # lowercase = text.lower()
+        # cleaned_text = lowercase.translate(str.maketrans(" ", " ", string.punctuation))
+        
+        # Tokenised_words = word_tokenize(cleaned_text, "english")
+        
+        # final_words = []
+        # for word in Tokenised_words:
+        #     if word not in stopwords.words("english"):
+        #         final_words.append(word)
+        
+        # # Initialize emotion counters
+        #     emotions_count = {
+        #         'anger': 0,
+        #         'anticipation': 0,
+        #         'disgust': 0,
+        #         'fear': 0,
+        #         'joy': 0,
+        #         'sadness': 0,
+        #         'surprise': 0,
+        #         'trust': 0,
+        #         'positive': 0,
+        #         'negative': 0
+        #     }
+
+        # # Analyze emotions in text
+        # for word in final_words:
+        #     if word in nrc_lexicon:
+        #         for emotion, association in nrc_lexicon[word].items():
+        #             if association == 1:
+        #                 emotions_count[emotion] += 1
+
+        # w = Counter(emotions_count)
+        # print(w)
+
+
+        # def overall_sentiment_analysis(sentiment_text):
+        #     score = SentimentIntensityAnalyzer().polarity_scores(sentiment_text)
+        #     neg = score['neg']
+        #     pos = score['pos']
+        #     if (pos > neg):
+        #         print("Overall sentiment is Positive")
+        #     elif (neg > pos):
+        #         print("Overall sentiment is Negative")
+        #     else:
+        #         print("Overall sentiment is Neutral")
+
+
+        # overall_sentiment_analysis(cleaned_text)
+
+        # fig , ax1 = plt.subplots()
+        # ax1.bar(w.keys() , w.values())
+        # fig.autofmt_xdate()
+        # plt.savefig('graph.png')
+        # plt.show()
+
+    else:
+        print("Invalid Choice. Please select Choice either 1 or 2.")
+
+    # result = {
+    #         'emotions_count': emotions_count
+    #     }
+    # print(json.dumps(result))
+
+if(__name__=="__main__"):
+    main()
---
+++ controller/analysisController.js
@@ -0,0 +1,104 @@
+const { PythonShell } = require( 'python-shell' )
+const fs = require( 'fs' );
+const path = require( 'path' );
+const { spawn } = require( 'child_process' );
+
+let emotions_count = {
+    'anger': 0,
+    'anticipation': 0,
+    'disgust': 0,
+    'fear': 0,
+    'joy': 0,
+    'sadness': 0,
+    'surprise': 0,
+    'trust': 0,
+    'positive': 0,
+    'negative': 0
+}
+
+const home = async ( req, res ) => {
+    const reqData = req.body;
+    // res.status(200).send("Home page of sentiment analysis");
+    const filePath = path.join( __dirname, '../public/front_end/Sentiment_Analysis_FrontEnd.html' );
+    console.log( req.url );
+    return res.status( 200 ).sendFile( filePath );
+}
+/*
+const analyzeContent = async ( req, res ) => {
+    const { analysiscontent } = req.body;
+    console.log( analysiscontent );
+    // Save the content to a file that the Python script will read
+    const filePath = "E:/abhishek/SA/Sentiment Analysis New 13-7-2024/Sentiment Analysis/Data/content.txt";
+    try {
+        await fs.promises.writeFile( filePath, analysiscontent );
+    } catch ( error ) {
+        console.error( 'Error writing file:', error );
+        return res.status( 500 ).send( 'Error writing file' );
+    }
+
+    const options = {
+        scriptPath: "E:/abhishek/SA/Sentiment Analysis New 13-7-2024/Sentiment Analysis",
+        args: [filePath],
+    }
+    // const pyshell = new PythonShell('SentimentAnalysis.py', options);
+    // Run the Python script
+    PythonShell.run( 'SentimentAnalysis.py', options, ( err, result ) => {
+        if ( err ) {
+            console.error( 'Error running Python script:', err );
+            return res.status( 500 ).send( 'Error running Python script' );
+        }
+        console.log(resu);
+        // Parse the Python script output
+        try {
+            const output = JSON.parse( result[0] );
+            console.log( 'Python script output:', output );
+            console.log( result );
+            for (let emotion in emotions_count) {
+                if (output[emotion] !== undefined) {
+                    emotions_count[emotion] = output[emotion];
+                }
+            }
+            console.log(emotions_count);
+            res.status(200).json({ emotions_count });
+           // const final_path = path.join( __dirname, '../public/front_end/Sentiment_Analysis_FrontEnd.html' )
+            //return res.status( 200 ).sendFile( final_path )
+        } catch ( parseError ) {
+            console.error( 'Error parsing Python script output:', parseError );
+            res.status( 500 ).send( 'Error parsing Python script output' );
+        }
+    } );
+};*/
+
+
+
+const analyzeContent = async (req, res) => {
+    const { analysiscontent } = req.body;
+    console.log('Received content for analysis:', analysiscontent);
+    const filePath = "E:/abhishek/SA/Sentiment Analysis New 13-7-2024/Sentiment Analysis/Data/content.txt";
+    try {
+        await fs.promises.writeFile( filePath, analysiscontent );
+    } catch ( error ) {
+        console.error( 'Error writing file:', error );
+        return res.status( 500 ).send( 'Error writing file' );
+    }
+    const pythonProcess = spawn('python', ["E:/abhishek/SA/Sentiment Analysis New 13-7-2024/Sentiment Analysis/SentimentAnalysis.py", analysiscontent]);
+
+    let outputData = '';
+    pythonProcess.stdout.on('data', (data) => {
+        outputData += data;
+
+        const filePath = path.join( __dirname, '../public/front_end/Sentiment_Analysis_FrontEnd.html' );
+       // return outputData;
+        return res.status(200).json(outputData);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`Error: ${data}`);
+        res.status(500).json({ error: 'Error analyzing content' });
+    });
+};
+
+
+// module.exports = { home,addContentInFile, analyzeContent ,...emotions_count};
+module.exports = { home, analyzeContent };
+// module.exports = output;
