0db238f4093eb7eabd2f3f08c179bdf320e671d5
Abhishek <sabaneabhishek0110@gmail.com>
2025-06-08 16:44:03 +0530

    Changes made in Sentiment_Analysyis_main_code.py

--- Backend/Sentiment_Analysis_main_code.py
+++ Backend/Sentiment_Analysis_main_code.py
@@ -168,5 +168,5 @@ async def analyze_text(request : TextRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 5000)) #yiou  can change port value for local test 
+    port = int(os.environ.get("PORT", 5000)) 
     uvicorn.run(app, host="0.0.0.0", port=port)
